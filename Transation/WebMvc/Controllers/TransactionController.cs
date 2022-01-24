ba49bc16a39fd3bbb36499fdfb8630a98ce1e0a8
Min Kaung Khant <minkaungkhant91@gmail.com>
2022-01-24 20:09:27 +0630

    Get Transaction API

---
+++ Transation/WebMvc/Controllers/TransactionController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Core;
+using Application;
+using Infrastructure;
+
+namespace WebMvc.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TransactionController : ControllerBase
+    {
+
+        private readonly ILogger<TransactionController> _logger;
+        private readonly ITransactionService _transactionService;
+
+        public TransactionController(ILogger<TransactionController> logger, ITransactionService transactionService)
+        {
+            _logger = logger;
+            _transactionService = transactionService;
+        }
+
+        [HttpGet]
+        public IActionResult GetTransactions()
+        {
+            var data = _transactionService.GetTransactions();
+
+            return Ok(data);
+        }
+
+        [HttpGet("getbycurrency")]
+        public IActionResult GetByCurrency([FromQuery(Name = "currency")] string currency)
+        {
+            var data = _transactionService.GetTransactionsByCurrency(currency);
+
+            return Ok(data);
+        }
+
+        [HttpGet("getbydate")]
+        public IActionResult GetByDateRange([FromQuery(Name = "startdate")] string startdate, [FromQuery(Name = "enddate")] string enddate)
+        {
+            var data = _transactionService.GetTransactionsByDateRange(startdate, enddate);
+
+            return Ok(data);
+        }
+
+        [HttpGet("getbystatus")]
+        public IActionResult GetByStatus([FromQuery (Name = "status")] string status)
+        {
+            var data = _transactionService.GetTransactionsByStatus(status);
+
+            return Ok(data);
+        }
+
+        //[HttpGet(Name = "GetByCurrency")]
+        //public IEnumerable<Transaction> GetByCurrency()
+        //{
+        //    return _transactionService.GetTransactions();
+        //}
+
+        //[HttpGet("GetByName")]
+        //public async Task<IActionResult> GetByName()
+        //{
+        //    return Ok();
+        //}
+    }
+}
--- Transation/WebMvc/Program.cs
+++ Transation/WebMvc/Program.cs
@@ -12,6 +12,15 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddApplicationService();
 builder.Services.AddInfrastructure();
 
+//FOR API
+builder.Services.AddCors();
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -20,9 +29,17 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+
+
+    //FOR API
+    app.UseDeveloperExceptionPage();
+    
 }
 
+
+app.MapControllers();
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
@@ -34,3 +51,4 @@ app.MapControllerRoute(
     pattern: "{controller=Upload}/{action=Index}/{id?}");
 
 app.Run();
+
